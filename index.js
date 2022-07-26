e264ce4d409bfe34148f57e9bd3679a591921405
Lauren Metz <Laurenmetz15@gmail.com>
2022-07-26 10:49:12 -0400

    commit tree f9804f573ec4ffae579c7dd51917861fbbe86f88

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+    }).then(resp => resp.json())
+    .then(function(obj) {
+        let newUser = document.createElement('p');
+        newUser.textContent = obj.id;
+        document.body.append(newUser);
+    })
+    .catch(function(error) {
+        let err = document.createElement('p');
+        err.innerHTML = error.message;
+        console.log(err);
+        document.body.append(err);
+    });
+};
